ad64f60847fc829772133087a967170a4ec29711
Jaeyeon Kim <amelia1023@naver.com>
2022-12-07 14:29:24 +0900

    remove /growthTest/ url prefix

--- config/urls.py
+++ config/urls.py
@@ -3,6 +3,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('growthTest/', include('growthTest.urls')),
+    path('', include('growthTest.urls')),
     path('user/', include('user.urls')),
 ]
