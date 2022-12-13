a266711de52890c37f0e5cd2b6c32f48985f54b2
Jaeyeon Kim <amelia1023@naver.com>
2022-12-13 15:36:35 +0900

    upload question&comment (new ver.)

--- growthTest/models.py
+++ growthTest/models.py
@@ -5,27 +5,20 @@ from django.contrib.postgres.fields import ArrayField
 # Create your models here.
 class Question(models.Model):
 
-    GROUP_CHOICES = (
-        ('A', 'A'),
-        ('B', 'B'),
-        ('C', 'C'),
-    )
-
     COLUMN_CHOICES = (
-        ('body', '신체'),
-        ('recognition', '인지'),
-        ('language', '언어'),
-        ('emotion', '정서'),
-        ('sociality', '사회성'),
-        ('autism', '자폐경향성'),
-        ('adhd', 'ADHD경향성'),
+        ('신체', '신체'),
+        ('인지', '인지'),
+        ('언어', '언어'),
+        ('정서', '정서'),
+        ('사회성', '사회성'),
+        ('자폐경향성', '자폐경향성'),
+        ('ADHD경향성', 'ADHD경향성'),
     )
     
     number = models.IntegerField(primary_key=True, help_text="질문번호")
     content = models.CharField(max_length=100, help_text="질문내용")
-    test_column = models.CharField(max_length=11, choices=COLUMN_CHOICES, help_text="규준표 컬럼")
-    group = models.CharField(max_length=1, choices=GROUP_CHOICES, help_text="질문그룹")
-
+    column = models.CharField(max_length=7, choices=COLUMN_CHOICES, help_text="규준표 컬럼")
+    age = models.IntegerField(null=True, blank=True, help_text="규준연령")
 
 class Answer(models.Model):
     child = models.OneToOneField(Child, on_delete=models.CASCADE, related_name="answer_child", help_text="검사 아동")
--- growthTest/upload_data.py
+++ growthTest/upload_data.py
@@ -19,9 +19,9 @@ def upload_questions():
         bulk_list.append(
             Question(
                 number = int(l[0]),
-                content = l[3],
-                test_column = l[2],
-                group = l[1]
+                content = l[1],
+                column = l[2],
+                age = int(l[3])
             )
         )
         
@@ -32,34 +32,49 @@ def upload_questions():
 # 종합소견 코멘트 업로드
 def upload_comments():
     Comment.objects.create(
-        classification = "발달수준",
+        classification = "발달지수",
         stage = "성숙",
         content = "현재 연령 수준에서 매우 적합하게 발달하고 있습니다. 일상에서 칭찬을 통해서 밝고 건강하게 자랄 수 있도록 해주세요."
     )
     Comment.objects.create(
-        classification = "발달수준",
+        classification = "발달지수",
         stage = "보통",
         content = "현재 연령 수준에서 적합하게 발달하고 있습니다. 일상에서 보다 칭찬을 많이 해주셔서 밝고 건강하게 자랄 수 있도록 해주세요."
     )
     Comment.objects.create(
-        classification = "발달수준",
+        classification = "발달지수",
         stage = "지연",
         content = "아동의 행동을 잘 관찰하시고, 관련한 전문가나 보다 세밀한 발달검사를 받아보실 것을 추천해 드립니다."
     )
     Comment.objects.create(
-        classification = "자폐/ADHD경향성",
+        classification = "자폐경향성",
         stage = "높음",
         content = "아동의 행동을 잘 관찰하시고, 관련한 전문가나 보다 세밀한 발달검사를 받아보실 것을 추천해 드립니다."
     )
     Comment.objects.create(
-        classification = "자폐/ADHD경향성",
+        classification = "자폐경향성",
         stage = "보통",
         content = "현재 연령 수준에서 적합하게 발달하고 있습니다. 일상에서 보다 칭찬을 많이 해주셔서 밝고 건강하게 자랄 수 있도록 해주세요."
     )
     Comment.objects.create(
-        classification = "자폐/ADHD경향성",
+        classification = "자폐경향성",
         stage = "낮음",
-        content = "현재 연령 수준에서 적합하게 잘 발달하고 있습니다. 일상에서  칭찬을 많이 해주셔서 밝고 건강하게 자랄 수 있도록 해주세요."
+        content = "현재 연령 수준에서 매우 적합하게 발달하고 있습니다. 일상에서 보다 칭찬을 많이 해주셔서 밝고 건강하게 성장할 수 있도록 해주세요."
+    )
+    Comment.objects.create(
+        classification = "ADHD경향성",
+        stage = "높음",
+        content = "아동의 행동을 잘 관찰하시고, 관련한 전문가나 보다 세밀한 발달검사를 받아보실 것을 추천해 드립니다."
+    )
+    Comment.objects.create(
+        classification = "ADHD경향성",
+        stage = "보통",
+        content = "현재 연령 수준에서 적합하게 발달하고 있습니다. 일상에서 보다 칭찬을 많이 해주셔서 밝고 건강하게 자랄 수 있도록 해주세요."
+    )
+    Comment.objects.create(
+        classification = "ADHD경향성",
+        stage = "낮음",
+        content = "현재 연령 수준에서 매우 적합하게 잘 발달하고 있습니다. 일상에서 칭찬을 많이 해주셔서 밝고 건강하게 자랄 수 있도록 해주세요."
     )
     print("!!comment upload success!!")
     return
