ff637c9539242f6cf1b54130b0fafd2dc1ece164
so9it <parkso9it@gmail.com>
2022-12-14 17:22:42 +0900

    [update] result.html/css 221214 sy.

--- growthTest/views.py
+++ growthTest/views.py
@@ -6,16 +6,24 @@ def home(request):
     return render(request, "growthTest/home.html")
 
 def test(request):
+    questions_arr = []
+    chunk = 5
     questions = Question.objects.all()
-    return render(request, "growthTest/test.html", {'question_list':questions})
+
+    for i in range(0, questions.count(), chunk):
+        questions_arr.append(questions[i:i+chunk])
+    
+    return render(request, "growthTest/test.html", {'question_arr':questions_arr})
 
 def result(request):
     return render(request, "growthTest/result.html")
 
 def bar_chart(request):
-    labels=["신체발달", "a", "b", "c", "d", "e", "f"]  # labels
-    data=[43, 60, 87, 69, 32, 12, 92]           # 계산 값(점수)
-
+    # labels=["발달지수", "자폐경향성", "ADHD 경향성"]  # labels
+    # data=[83, 35, 12]      # 계산 값(점수) 수정
+    labels=[""]     # 라벨링은 따로,,,,?
+    data=[75]
+    
     return JsonResponse(data={
         'labels': labels,
         'data': data,
