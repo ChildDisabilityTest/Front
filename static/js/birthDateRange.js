51c067f1aefac01fb278638a9b8c6a98a4042918
so9it <parkso9it@gmail.com>
2023-01-11 09:50:07 +0900

    [fix] userinfo birthdate range. 230111 sy.

--- static/js/birthDateRange.js
+++ static/js/birthDateRange.js
@@ -6,15 +6,30 @@ $(document).ready(function () {
   const tm = today.getMonth();
   const td = today.getDate();
 
-  let minDate = new Date(ty - 7, tm, td); // 만 1세 기준 날짜 계산
-  minDate.setDate(minDate.getDate() + 1); // 만 6세 기준 날짜 계산
+  // 만 1세 기준 날짜 계산
+  let minDate = new Date(ty - 7, tm, td);
+  minDate.setDate(minDate.getDate() + 1);
   // alert(minDate);
 
+  // max, min 날짜 구하기
+  const maxy = ty - 1;
+  let maxm = tm + 1;
+  const maxd = td;
   const miny = minDate.getFullYear();
-  const minm = minDate.getMonth();
+  let minm = minDate.getMonth() + 1;
   const mind = minDate.getDate();
 
+  // yyyy-mm-dd 포맷에 맞추기 위해서 한 자릿수의 경우 0 붙이기
+  if (maxm <= 9) {
+    maxm = "0" + maxm.toString();
+    // alert(maxm);
+  }
+  if (minm <= 9) {
+    minm = "0" + minm.toString();
+    // alert(minm);
+  }
+
   let childBirth = document.getElementById("childBirthDate");
-  childBirth.setAttribute("max", `${ty - 1}-${tm + 1}-${td}`);
-  childBirth.setAttribute("min", `${miny}-${minm + 1}-${mind}`);
+  childBirth.setAttribute("max", `${maxy}-${maxm}-${maxd}`);
+  childBirth.setAttribute("min", `${miny}-${minm}-${mind}`);
 });
